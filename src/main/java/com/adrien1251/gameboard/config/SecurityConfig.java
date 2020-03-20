7aac429cc213bdec64ff103ff2bbcdf3a6ac5065
deblocka <fdbxf8ixfn>
2020-03-20 14:31:08 +0100

    feat(crosso): configure security

---
+++ src/main/java/com/adrien1251/gameboard/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.adrien1251.gameboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override 
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.cors().and().authorizeRequests()
+                .requestMatchers(CorsUtils:: isPreFlightRequest).permitAll()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
